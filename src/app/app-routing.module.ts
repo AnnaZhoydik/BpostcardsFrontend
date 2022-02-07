d2152bed0d0a2c8a83749d48d759835ee23fe10b
Anna Zhoydik <anka22@msn.com>
2022-02-07 17:06:45 +0200

    New components. Making header links(Postcards,Gallery,Qustions,Sign in, Registration) work.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+import {QuestionsComponent} from "./questions/questions.component";
+import {PostcardsComponent} from "./postcards/postcards.component";
+import {ContactComponent} from "./contact/contact.component";
+import {GalleryComponent} from "./gallery/gallery.component";
+import {SigninComponent} from "./signin/signin.component";
+import {SignupComponent} from "./signup/signup.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'questions', component: QuestionsComponent},
+  {path: 'postcards', component: PostcardsComponent},
+  {path: 'contacts',component: ContactComponent},
+  {path: 'gallery',component: GalleryComponent},
+  {path: 'signin',component: SigninComponent},
+  {path: 'signup',component: SignupComponent},
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { ContactComponent } from './contact/contact.component';
+import { PostcardsComponent } from './postcards/postcards.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    QuestionsComponent,
+    ContactComponent,
+    PostcardsComponent,
+    GalleryComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
